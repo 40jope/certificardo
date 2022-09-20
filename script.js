bc38d6cd373012890b1e44cd9f1f08c692a9a7be
40jope <114001775+40jope@users.noreply.github.com>
2022-09-20 11:39:19 -0300

    adicionei arquivos do certifiard (certo dessa vez)

---
+++ script.js
@@ -0,0 +1,12 @@
+function mudaTema() {
+  document.body.classList.toggle("dark");
+}
+
+function instagram() {
+  var url = "https://www.instagram.com/kanyewest/";
+  var abrir = window.open(url, "_blank");
+}
+
+function twitter() {}
+
+function codepen() {}
